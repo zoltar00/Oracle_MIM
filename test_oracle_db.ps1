9f61cafce263bf8bc100bde90d84cf1ef937417a
zoltar00 <32526436+zoltar00@users.noreply.github.com>
2019-05-28 22:02:30 +0200

    Initial Commit

---
+++ test_oracle_db.ps1
@@ -0,0 +1,17 @@
+﻿Add-Type -Path "C:\u01\apps\ODAC\odp.net\managed\common\Oracle.ManagedDataAccess.dll"
+$reader=$false
+$username = Read-Host -Prompt "Enter database username"
+$password = Read-Host -Prompt "Enter database password"
+$datasource = Read-Host -Prompt "Enter database TNS name (SID)"
+$query = "SELECT first_name,last_name FROM users"
+#$query = "SELECT 'Hello World' Greeting from dual"
+$connectionString = 'User Id=' + $username + ';Password=' + $password + ';Data Source=' + $datasource
+$connection = New-Object Oracle.ManagedDataAccess.Client.OracleConnection($connectionString)
+$connection.open()
+$command=$connection.CreateCommand()
+$command.CommandText=$query
+$reader=$command.ExecuteReader()
+While ($reader.Read()) {
+    $reader.GetString(0) + ' ' + $reader.GetString(1)
+}
+$connection.Close()
